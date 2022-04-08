fb7942e23718ae32d9d18a3ea6aae18e0c7bfc4f
Your Name <you@example.com>
2022-04-08 13:54:42 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints phrase
+ * main - prints phrase "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return - 1
  */
 int main(void)
