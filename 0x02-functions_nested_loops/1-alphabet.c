6d29c496aaa0ae98758a95d5041fb60fae5b04e4
Your Name <you@example.com>
2022-04-13 05:34:31 -0700

    c1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include"main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char alpha;
-	for (alpha = 'a'; alpha <= 'z'; alpha++);
-			_putchar(alpha);
-			_putchar('\n');
-
+	int i;
+	for (i = 'a'; i <= 'z'; i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
 }
