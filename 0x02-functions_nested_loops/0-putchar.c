7baa0610c1e927077e8d1528e07e457699a09916
Your Name <you@example.com>
2022-04-13 02:47:18 -0700

    c1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,59 @@
-void hello(*char name) {
+#include "holberton.h"
 
-	   printf("Hello %s",name);
+/**
+ *
+ * *print_diagonal - prints backward slashes diagonally
+ *
+ * *@n: how long the line is
+ *
+ * *Description: Diagonal line printer
+ *
+ * *Return: nothing
+ *
+ * */
+
+void print_diagonal(int n)
+
+{
+
+	int i, j;
+
+	if (n > 0)
+
+	{
+
+		for (i = 0; i < n; i++)
+
+		{
+
+			for (j = 0; j <= i; j++)
+
+			{
+
+				_putchar(' ');
+
+				if (j == i)
+
+				{
+
+					_putchar('\\');
+
+				}
+
+			}
+
+			_putchar('\n');
+
+		}
+
+	}
+
+	else
+
+	{
+
+		_putchar('\n');
+
+	}
 
 }
