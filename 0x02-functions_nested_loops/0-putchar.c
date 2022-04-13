6d5f75a40aeacbc864f9ac9f4240c116a6c9237a
Your Name <you@example.com>
2022-04-13 02:55:59 -0700

    c1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,59 +1,6 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- *
- * *print_diagonal - prints backward slashes diagonally
- *
- * *@n: how long the line is
- *
- * *Description: Diagonal line printer
- *
- * *Return: nothing
- *
- */
-
-void print_diagonal(int n)
-
-{
-
-	int i, j;
-
-	if (n > 0)
-
-	{
-
-		for (i = 0; i < n; i++)
-
-		{
-
-			for (j = 0; j <= i; j++)
-
-			{
-
-				_putchar(' ');
-
-				if (j == i)
-
-				{
-
-					_putchar('\\');
-
-				}
-
-			}
-
-			_putchar('\n');
-
-		}
-
-	}
-
-	else
-
-	{
-
-		_putchar('\n');
-
-	}
-
+int main() {
+	    printf("_putchar");
+	        return 0;
 }
